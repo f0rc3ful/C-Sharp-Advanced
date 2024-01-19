3468aa27505365996bac04a0f59e23dec3bcbe51
Martin <146632124+f0rc3ful@users.noreply.github.com>
2024-01-19 15:48:17 +0200

    7

---
+++ 3. Dictionaries Advanced/7. Parking Lot/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input;
+        HashSet<string> parkingLot = new HashSet<string>();
+        while ((input = Console.ReadLine()) != "END") 
+        {
+            string[] command = input.Split(", ");
+            if (command[0] == "IN")
+            {
+                parkingLot.Add(command[1]);
+            }
+            else if (command[0] == "OUT")
+            {
+                parkingLot.Remove(command[1]);
+            }
+        }
+
+        if (parkingLot.Count == 0)
+        {
+            Console.WriteLine("Parking Lot is Empty");
+        }
+        else
+        {
+            Console.WriteLine(string.Join("\n", parkingLot));
+        }
+
+    }
+}
+
+
+//CA9999TT
+
+//CA2844AA
+//CA9876HH
+//CA2822UU
