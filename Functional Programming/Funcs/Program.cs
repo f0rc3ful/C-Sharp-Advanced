4e93c7ccf67f225d9645a4dc2b9278e690fd0ea2
marin <f0rc3ful@protonmail.com>
2024-01-27 20:09:57 +0200

    func update

--- Functional Programming/Funcs/Program.cs
+++ Functional Programming/Funcs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
@@ -6,7 +7,8 @@ class Program
     {
         Console.WriteLine("Keep on entering integers to see if they are even or odd numbers. \nOnce you are done, you can enter \"done\" (case insensitive) and the program will end.");
         string input;
-        Func<int, bool> isOdd = x => x % 2 == 0;
+        //Func<int, bool> isOdd = x => x % 2 == 0;
+        Func<int, bool> funcOddOrNot = isOdd;
         while ((input = Console.ReadLine()).ToLower() != "end")
         {
             if (!int.TryParse(input, out int result))
@@ -15,13 +17,22 @@ class Program
             }
             else if (isOdd(int.Parse(input)))
             {
-                Console.WriteLine($"{input} is an even number.");
+                Console.WriteLine($"{input} is an even number");
             }
+            //else if (isOdd(int.Parse(input)))
+            //{
+            //    Console.WriteLine($"{input} is an even number");
+            //}
             else
             {
                 Console.WriteLine($"{input} is not an odd number.");
             }
-            
+        }
+        Console.WriteLine("Program closing...");
+
+        static bool isOdd(int x)
+        {
+            return x % 2 == 0;
         }
 
     }
