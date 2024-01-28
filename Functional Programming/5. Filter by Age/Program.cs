3c401b7cd7c70dbcba3cb1a6e64c2df3dc7f94b4
marin <f0rc3ful@protonmail.com>
2024-01-28 19:53:12 +0200

    new

---
+++ Functional Programming/5. Filter by Age/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;  
+    }
+
+} 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<Person> people = ReadPeople(); 
+        string condition = Console.ReadLine(); //•	Condition - "younger"(<) or "older"(>=)
+        int ageThreshold = int.Parse(Console.ReadLine()); //•	Age threshold -integer\
+        string format = Console.ReadLine(); //•	Format - "name", "age" or "name age"\
+        if (condition == "older")
+        {
+            people = people.Where(x => x.Age >= ageThreshold).ToList();
+        }
+        else if (condition == "younger")
+        {
+            people = people.Where(x => x.Age < ageThreshold).ToList();
+        }
+
+        foreach (Person person in people)
+        {
+            if (format == "age")
+            {
+                Console.WriteLine(person.Age);
+            }
+            else if (format == "name")
+            {
+                Console.WriteLine(person.Name);
+            }
+            else if (format.Length == 2)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+
+    }
+
+    static void ReadPeople()
+    { 
+    
+    }
+}
--- Functional Programming/Functional Programming/Program.cs
+++ Functional Programming/Functional Programming/Program.cs
@@ -3,7 +3,19 @@
 class Program
 {
     static void Main(string[] args)
-    { 
-    
+    {
+        Func<int, int, int> calculator = Multiplicator;
+        Console.WriteLine(calculator(2, 3));
+        Console.WriteLine(calculator(3, 4));
+        Console.WriteLine(calculator(4, 5));
+    }
+
+    static int Multiplicator(int a, int b)
+    {
+        return a * b;
+    }
+    static int Sum(int a, int b)
+    {
+        return a + b;
     }
 }
