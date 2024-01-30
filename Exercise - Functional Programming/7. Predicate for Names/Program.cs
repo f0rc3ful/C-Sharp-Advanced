c6c68a9b84c22dbcb91192338a0c211e63b57488
marin <f0rc3ful@protonmail.com>
2024-01-30 20:59:06 +0200

    7.

---
+++ Exercise - Functional Programming/7. Predicate for Names/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Channels;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int maxLength = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine().Split(" ");
+        Predicate<string> predicate = name => name.Length <= maxLength;
+        names = names.Where(name => predicate(name)).ToArray();
+        Action<string[]> printer = name => Console.WriteLine(string.Join("\n", name));
+        printer(names);
+    }
+}
