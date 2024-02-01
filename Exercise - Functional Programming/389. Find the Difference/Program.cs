b37ea4db9e185e4ebed75bc2be4a71f0f1d6cc58
marin <f0rc3ful@protonmail.com>
2024-02-01 15:35:53 +0200

    leet

---
+++ Exercise - Functional Programming/389. Find the Difference/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        string s = Console.ReadLine();
+        string t = Console.ReadLine();
+        Console.WriteLine(FindTheDifference(s,t));
+    }
+    static char FindTheDifference(string s, string t)
+    {
+        List<char> list = t.ToList();
+        int counter = 0;
+        while (list.Count > 1)
+        {
+            if (list.Contains(s[counter]))
+            {
+                list.Remove(s[counter]);
+            }
+            counter++;
+        }
+        return char.Parse(string.Join("", list));
+    }
+}
