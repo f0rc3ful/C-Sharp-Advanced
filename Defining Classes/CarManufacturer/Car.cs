0d1fbcefd5e14e0a47f73bf3725244e5fc6be10c
marin <f0rc3ful@protonmail.com>
2024-02-03 13:41:03 +0200

    fixed engine and tire classes

--- Defining Classes/CarManufacturer/Car.cs
+++ Defining Classes/CarManufacturer/Car.cs
@@ -15,12 +15,17 @@ namespace CarManufacturer
 		private int year;
 		private double fuelQuantity;
 		private double fuelConsumption;
+		private Engine engine;
+		private Tire[] tires;
+
 
 		public string Make { get; set; }
 		public string Model { get; set; }
 		public int Year { get; set; }
 		public double FuelQuantity { get; set; }
 		public double FuelConsumption { get; set; }
+		public Engine Engine { get; set; }
+		public Tire[] Tires {get; set;}
 		public Car()
 		{
 			this.Make = "VW";
@@ -43,6 +48,13 @@ namespace CarManufacturer
 			this.FuelConsumption = fuelConsumption;
 
 		}
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+            : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+
+        }
         public void Drive(double distance)
 		{
 			double outcome = fuelQuantity - distance * fuelConsumption;
---
+++ Defining Classes/CarManufacturer/Engine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        private int horsePower;
+        private double cubicCapacity;
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.horsePower = horsePower;
+            this.cubicCapacity = cubicCapacity;
+        }
+    }
+}
