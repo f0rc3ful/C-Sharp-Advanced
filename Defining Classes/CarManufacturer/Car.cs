bb6a4dbca0448b1ec85a06dc9bf1330316f26340
marin <f0rc3ful@protonmail.com>
2024-02-03 17:23:55 +0200

    Class structure changes

--- Defining Classes/CarManufacturer/Car.cs
+++ Defining Classes/CarManufacturer/Car.cs
@@ -8,46 +8,66 @@ using System.Threading.Tasks;
 
 namespace CarManufacturer
 {
-	public class Car
-	{
-		private string make;
-		private string model;
-		private int year;
-		private double fuelQuantity;
-		private double fuelConsumption;
-		private Engine engine;
-		private Tire[] tires;
+    public class Car
+    {
+        private Engine engine;
+        private Tire[] tires;
 
-
-		public string Make { get; set; }
-		public string Model { get; set; }
-		public int Year { get; set; }
-		public double FuelQuantity { get; set; }
-		public double FuelConsumption { get; set; }
-		public Engine Engine { get; set; }
-		public Tire[] Tires {get; set;}
-		public Car()
-		{
-			this.Make = "VW";
-			this.Model = "Golf";
-			this.Year = 2025;
-			this.FuelQuantity= 200;
-			this.FuelConsumption = 10;
-		}
-        public Car(string make, string model, int year)
-			: this()
+        private string make;
+        public string Make
         {
-			this.Make = make;
-			this.Model = model;
-			this.Year = year;
+            get { return this.make; }
+            set { this.make = value; }
         }
-		public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
-			: this(make, model, year)
-		{
-			this.FuelQuantity = fuelQuantity;
-			this.FuelConsumption = fuelConsumption;
+        private string model;
 
-		}
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        private int year;
+
+        public int Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+
+        private double fuelQuantity;
+
+        public double FuelQuantity
+        {
+            get { return this.fuelQuantity; }
+            set { this.fuelQuantity = value; }
+        }
+
+        public double FuelConsumption { get; set; }
+        public Engine Engine { get; set; }
+        public Tire[] Tires { get; set; }
+        public Car()
+        {
+            this.make = "VW";
+            this.model = "Golf";
+            this.year = 2025;
+            this.fuelQuantity = 200;
+            this.fuelConsumption = 10;
+        }
+        public Car(string make, string model, int year)
+            : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+
+        }
         public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
             : this(make, model, year, fuelQuantity, fuelConsumption)
         {
@@ -56,22 +76,22 @@ namespace CarManufacturer
 
         }
         public void Drive(double distance)
-		{
-			double outcome = fuelQuantity - distance * fuelConsumption;
+        {
+            double outcome = fuelQuantity - distance * fuelConsumption;
 
-			if (outcome > 0)
-			{
-				this.fuelQuantity -= distance * this.fuelConsumption;
-			}
-			else
-			{
-				Console.WriteLine("Not enough fuel to perform this trip!");
-			}
-		}
+            if (outcome > 0)
+            {
+                this.fuelQuantity -= distance * this.fuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
 
-		public string WhoAmI()
-		{
-			return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}";
-		}
-	}
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}";
+        }
+    }
 }
