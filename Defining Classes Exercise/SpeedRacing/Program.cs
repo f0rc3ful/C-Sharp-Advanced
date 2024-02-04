0862c4f0fbde79f075f80e7991933eb471d06b44
marin <f0rc3ful@protonmail.com>
2024-02-05 01:19:19 +0200

    f

---
+++ Defining Classes Exercise/SpeedRacing/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SpeedRacing
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                Car car = new Car(input[0], double.Parse(input[1]), double.Parse(input[2]));
+            }
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] 
+            }
+        }
+    }
+}
