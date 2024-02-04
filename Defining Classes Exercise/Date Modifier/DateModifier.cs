def4e757527e89c9dd5881274657e49b1e948c55
marin <f0rc3ful@protonmail.com>
2024-02-04 16:13:38 +0200

    date modifier

---
+++ Defining Classes Exercise/Date Modifier/DateModifier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Modifier
+{
+    public class DateModifier
+    {
+		private int differenceInDays;
+		private DateTime firstDate;
+		private DateTime secondDate;
+
+
+		public DateModifier(string firstDate, string secondDate)
+		{
+			this.firstDate = DateTime.Parse(firstDate);
+			this.secondDate = DateTime.Parse(secondDate);
+        }
+
+		public int DifferenceInDays
+		{
+			get
+			{
+				return differenceInDays = Math.Abs((int)(firstDate - secondDate).TotalDays);
+			}
+			set { differenceInDays = value; }
+		}
+	}
+}
---
+++ Defining Classes Exercise/Date Modifier/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Date_Modifier
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            DateModifier difference = new DateModifier(input1, input2);
+            Console.WriteLine(difference.DifferenceInDays);
+        }
+    }
+        
+}
