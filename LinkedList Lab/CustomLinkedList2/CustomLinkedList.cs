1e8a41f594a87fe9bcfd856aadf49325b3f60ed1
Martin <146632124+f0rc3ful@users.noreply.github.com>
2024-02-05 15:54:16 +0200

    added description of custom linked list

---
+++ LinkedList Lab/CustomLinkedList2/CustomLinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedList2
+{
+    public class CustomLinkedList
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+
+        public void InsertAsHead(int value)
+        {
+            Node newNode = new Node(value);
+            if (Head == null)
+            { 
+                Head = newNode;
+                Tail = newNode;
+            }
+            newNode.Next = Head;
+            Head.Previous = newNode;
+            Head = newNode;
+        }
+
+        public void InsertAsTail(int value) 
+        {
+            Node newNode = new Node(value);
+            if (Tail == null)
+            {
+                newNode.Previous = null;
+                newNode.Next = null;
+            }
+            newNode.Previous = Tail;
+            Tail.Next = newNode;
+            Tail = newNode;
+        }
+
+        public void RemoveTail()
+        {
+            Tail.Next = null;
+            Tail = Tail.Previous;
+        }
+
+        public void RemoveHead()
+        {
+            Head = Head.Next;
+            Head.Previous = null;
+        }
+    }
+}
---
+++ LinkedList Lab/CustomLinkedList2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedList2
+{
+    class Program
+    {
+        static void Main()
+        { 
+            CustomLinkedList list = new CustomLinkedList();
+            list.InsertAsHead(333);
+            list.InsertAsHead(222);
+            list.InsertAsHead(111);
+            Console.WriteLine($"1st head: {list.Head.Value}");
+            Console.WriteLine($"1st tail: {list.Tail.Value}");
+
+            list.InsertAsTail(444);
+            Console.WriteLine($"2nd tail: {list.Tail.Value}");
+
+            list.RemoveTail();
+            Console.WriteLine($"3rd tail: {list.Tail.Value}");
+
+            list.RemoveTail();
+            Console.WriteLine($"4th tail: {list.Tail.Value}");
+
+            list.InsertAsHead(555);
+            Console.WriteLine($"2nd head: {list.Head.Value}");
+
+            list.RemoveHead();
+            Console.WriteLine($"back to 1st head: {list.Head.Value}");
+
+        }
+    }
+}
