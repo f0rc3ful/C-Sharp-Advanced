dcaea0f321f8902f1d29eab7d25a5cbae6d7867a
Martin <146632124+f0rc3ful@users.noreply.github.com>
2024-02-05 15:07:07 +0200

    custom linked list

---
+++ LinkedList Lab/CustomLinkedList/CustomLinkedList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedList
+{
+    public class CustomLinkedList
+    {
+         public Node Head { get; set; }
+         public Node Tail { get; set; }
+
+        public void AddLast(int value)
+        {
+            Node newNode = new Node(value);
+            if (Head == null)
+            {
+                Head = newNode;
+                Tail = newNode;
+                return;
+            }
+            newNode.Previous = Tail;
+            Tail.Next = newNode;
+            Tail = newNode;
+        }
+
+        public void AddFirst(int value)
+        {
+            Node newNode = new Node(value);
+            if (Head == null)
+            {
+                Head = newNode;
+                Tail = newNode;
+                return;
+            }
+            newNode.Next = Head;
+            Head.Previous = newNode;
+            Head = newNode;
+        }
+    }
+}
---
+++ LinkedList Lab/CustomLinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomLinkedList
+{
+    class Program
+    {
+        static void Main()
+        {
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.AddLast(1);
+            customLinkedList.AddLast(2);
+            Node head = customLinkedList.Head;
+            Console.WriteLine($"firt head: {head.Value}");
+            Node tail = customLinkedList.Tail;
+            Console.WriteLine($"first tail: {tail.Value}");
+
+            customLinkedList.AddFirst(3);
+
+            head = customLinkedList.Head;
+            Console.WriteLine($"second head: {head.Value}");
+            tail = customLinkedList.Tail;
+            Console.WriteLine($"second tail: {tail.Value}");
+        }
+    }
+}
