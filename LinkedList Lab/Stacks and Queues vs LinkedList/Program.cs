60fdad0b679b0c9f92625e8aa8b1be329d7341b8
Martin <146632124+f0rc3ful@users.noreply.github.com>
2024-02-05 12:51:18 +0200

    asd

---
+++ LinkedList Lab/Stacks and Queues vs LinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿Queue<string> queue =  new Queue<string>();
+LinkedList<string> linkedListQ= new LinkedList<string>();
+
+queue.Enqueue("Toshko");
+linkedListQ.AddLast("Toshko");
+
+queue.Enqueue("Niki");
+linkedListQ.AddLast("Niki");
+
+queue.Enqueue("Moni");
+linkedListQ.AddLast("Moni");
+
+while (queue.Count > 0)
+{ 
+    string served = queue.Dequeue();
+    Console.WriteLine($"Queue: {served}");
+}
+
+LinkedListNode<string> person = linkedListQ.First;
+while (person != null)
+{
+    Console.WriteLine($"Linked list: {person.Value}");
+    linkedListQ.RemoveFirst();
+    person = linkedListQ.First;
+}
+
