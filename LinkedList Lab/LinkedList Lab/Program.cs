3f1e05192b9d1d1524fc07775ae2106de4bcf34d
Martin <146632124+f0rc3ful@users.noreply.github.com>
2024-02-05 12:37:47 +0200

    linkedlist intro

--- LinkedList Lab/LinkedList Lab/Program.cs
+++ LinkedList Lab/LinkedList Lab/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿LinkedList<int> linkedList = new LinkedList<int>();
+linkedList.AddFirst(5);
+linkedList.AddFirst(0);
+linkedList.AddFirst(5);
+linkedList.AddFirst(2);
+
+LinkedListNode<int> node = linkedList.First;
+while (node != null)
+{
+    Console.WriteLine(node.Value);
+    node = node.Next;
+}
