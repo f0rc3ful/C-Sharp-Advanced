cac1b72cd2164d1ee2f0e5c0d1d4840987edfa49
marin <f0rc3ful@protonmail.com>
2024-02-06 23:09:46 +0200

    car3

---
+++ DefiningClassesRerun/CarConstructors/Car.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+		private string make;
+
+		public string Make
+		{
+			get { return make; }
+			set { make = value; }
+		}
+
+		private string model;
+
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+		private int year;
+
+		public int Year
+		{
+			get { return year; }
+			set { year = value; }
+		}
+		private double fuelQuantity;
+
+		public double FuelQuantity
+		{
+			get { return fuelQuantity; }
+			set { fuelQuantity = value; }
+		}
+		private double fuelConsumption;
+
+		public double FuelConsumption
+		{
+			get { return fuelConsumption; }
+			set { fuelConsumption = value; }
+		}
+
+		public Car()
+		{
+			this.Make = "VW";
+			this.Model = "Golf";
+			this.Year = 2025;
+			this.FuelQuantity = 200;
+			this.fuelConsumption = 10;
+		}
+
+		public Car(string make, string model, int year)
+			: this()
+		{
+			Make = make;
+			Model = model;
+			Year = year;
+		}
+
+		public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+			: this(make, model, year)
+		{
+			FuelConsumption = fuelConsumption;
+			FuelQuantity = fuelQuantity;
+		}
+		public void Drive(double distance)
+		{
+			if (FuelQuantity - distance * FuelConsumption > 0)
+			{
+				FuelQuantity -= distance * FuelConsumption;
+			}
+			else
+			{
+				Console.WriteLine("Not enough fuel to perform this trip");
+			}
+		}
+
+		public string WhoAmI()
+		{ 
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine($"Make: {Make}");
+            sb.AppendLine($"Model: {Model}");
+            sb.AppendLine($"Year: {Year}");
+            sb.AppendLine($"Fuel: {FuelQuantity:F2}");
+			return sb.ToString();
+        }
+
+	}
+}
---
+++ DefiningClassesRerun/CarConstructors/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car1 = new Car();
+            Car car2 = new Car("Hyundai", "Getz", 2005);
+            Car car3 = new Car("Kia", "K7", 2023, 200, 8);
+
+            Console.WriteLine(car1.WhoAmI());
+            Console.WriteLine(car2.WhoAmI());
+            Console.WriteLine(car3.WhoAmI());
+        }
+    }
+}
