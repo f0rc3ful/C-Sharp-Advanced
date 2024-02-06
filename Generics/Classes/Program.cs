acf5507e1aa69cf6bf119e2c5aea2689bdff2555
marin <f0rc3ful@protonmail.com>
2024-02-06 13:57:02 +0200

    generic methods

---
+++ Generics/Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿CustomGeneric<string> genericString = new CustomGeneric<string>();
+genericString.AddElement("helou");
+genericString.Name = "string";
+
+CustomGeneric<DateTime> genericDateTime = new CustomGeneric<DateTime>();
+genericDateTime.Name = DateTime.Now;
+genericDateTime.AddElement(DateTime.Today);
+
+public class CustomGeneric<T>
+{
+    public T x;
+    public T Name { get; set; }
+
+    public List<T> list = new List<T>();
+
+    public void AddElement(T element)
+    { 
+        list.Add(element);
+        Console.WriteLine(element);
+    }
+}
---
+++ Generics/GenericMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine(string.Join(", ", GenerateElements(5, 10)));
+Console.WriteLine(string.Join(", ", GenerateElements("five", 10)));
+Console.WriteLine(string.Join(", ", GenerateElements(DateTime.Today, 10)));
+
+
+static List<T> GenerateElements<T> (T element, int n)
+{
+    List<T> list = new List<T>();
+    for (int i = 0; i < n; i++)
+    {
+        list.Add(element);
+    }
+    return list;
+}
