9b5924100d598b180551546960582e170f64c73f
marin <f0rc3ful@protonmail.com>
2024-02-06 22:01:31 +0200

    recursion

---
+++ DefiningClassesRerun/DefiningClassesRerun/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Counter(n);
+    }
+
+    static void Counter(int n)
+    {
+        ////if (n == 0)
+        ////{
+        ////    return;
+        ////}
+        Console.WriteLine(n);
+        Counter(n - 1);
+    }
+}
---
+++ GenericsExercise/GenericsExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfString
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+            for (int i = 0; i < n; i++)
+            {
+                box.Add(Console.ReadLine());
+            }
+            Console.WriteLine(box);
+        }
+    }
+}
