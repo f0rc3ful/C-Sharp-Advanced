d5aa2afa52039c053339a1cb6115fae8fdf73073
marin <f0rc3ful@protonmail.com>
2024-02-08 14:43:17 +0200

    enum

---
+++ DefiningClassesRerun/FieldsPropertiesConstuctorswNiki/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldsPropertiesConstructorswNiki
+{
+    public class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public Person(string nameValue, int ageValue)
+        {
+            if (char.IsLower(nameValue[0]))
+            {
+                Name = char.ToUpper(nameValue[0]) + nameValue.Substring(1);
+            }
+            else
+            {
+                Name = nameValue;
+            }
+            if (ageValue < 0 || ageValue > 140)
+            {
+                Age = 0;
+            }
+            else
+            {
+                Age = ageValue;
+            }
+        }
+
+    }
+}
---
+++ DefiningClassesRerun/FieldsPropertiesConstuctorswNiki/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldsPropertiesConstructorswNiki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("zara", -1);
+            Console.WriteLine(person1.Name);
+            Console.WriteLine(person1.Age);
+        }
+    }
+}
