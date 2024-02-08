1f40661c4338b1b085ca45324dc944577cc97007
marin <f0rc3ful@protonmail.com>
2024-02-08 23:42:11 +0200

    9 done

--- DefiningClassesExerciseRerun/09PokemonTrainer/Program.cs
+++ DefiningClassesExerciseRerun/09PokemonTrainer/Program.cs
@@ -1,2 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09PokemonTrainer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, Trainer> trainersList = new Dictionary<string, Trainer>();
+            string input;
+            while ((input = Console.ReadLine()) != "Tournament")
+            {
+                string[] inputArr = input.Split(" ");
+                string trainerName = inputArr[0];
+                string pokemonName = inputArr[1];
+                string pokemonElement = inputArr[2];
+                int pokemonHealth = int.Parse(inputArr[3]);
+                Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+                if (!trainersList.ContainsKey(trainerName))
+                {
+                    Trainer trainer = new Trainer(trainerName, new List<Pokemon> { pokemon });
+                    trainersList.Add(trainerName, trainer);
+                }
+                else
+                {
+                    trainersList[trainerName].Pokemon.Add(pokemon);
+                }
+            }
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input == "Fire")
+                {
+                    foreach (var trainer in trainersList.Values)
+                    {
+                        if (trainer.Pokemon.Any(pokemon => pokemon.Element == "Fire"))
+                        {
+                            trainer.Badges++;
+                        }
+                        else
+                        {
+                            trainer.Pokemon.Select(pokemon => pokemon.Health -= 10);
+                        }
+                        trainer.Pokemon.RemoveAll(pokemon => pokemon.Health <= 0);
+                    }
+                }
+                else if (input == "Water")
+                {
+                    foreach (var trainer in trainersList.Values)
+                    {
+                        if (trainer.Pokemon.Any(pokemon => pokemon.Element == "Water"))
+                        {
+                            trainer.Badges++;
+                        }
+                        else
+                        {
+                            trainer.Pokemon.Select(pokemon => pokemon.Health -= 10);
+                        }
+                        trainer.Pokemon.RemoveAll(pokemon => pokemon.Health <= 0);
+                    }
+                }
+                else if (input == "Electricity")
+                {
+                    foreach (var trainer in trainersList.Values)
+                    {
+                        if (trainer.Pokemon.Any(pokemon => pokemon.Element == "Electricity"))
+                        {
+                            trainer.Badges++;
+                        }
+                        else
+                        {
+                            trainer.Pokemon.Select(pokemon => pokemon.Health -= 10);
+                        }
+                        trainer.Pokemon.RemoveAll(pokemon => pokemon.Health <= 0);
+                    }
+                }
+
+            }
+
+            trainersList = trainersList.OrderByDescending(trainer => trainer.Value.Badges).ToDictionary(pair => pair.Key, pair => pair.Value);
+            foreach (var trainer in trainersList)
+            {
+                Console.WriteLine($"{trainer.Key} {trainer.Value.Badges} {trainer.Value.Pokemon.Count()}");
+            }
+        }
+    }
+}
--- DefiningClassesExerciseRerun/09PokemonTrainer/Trainer.cs
+++ DefiningClassesExerciseRerun/09PokemonTrainer/Trainer.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace _09PokemonTrainer
 {
-    internal class Trainer
+    public class Trainer
     {
-    }
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		private int badges;
+
+		public int Badges
+		{
+			get { return badges; }
+			set { badges = value; }
+		}
+		private List<Pokemon> pokemon;
+
+		public List<Pokemon> Pokemon
+		{
+			get { return pokemon; }
+			set { pokemon = value; }
+		}
+
+		public Trainer(string name, List<Pokemon> pokemon)
+		{
+			Name = name;
+			Badges = 0;
+			Pokemon = pokemon;
+		}
+
+	}
 }
