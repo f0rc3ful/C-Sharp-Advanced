4336760ea9a966d7b5ea01f11790563fcfa2f77e
marin <f0rc3ful@protonmail.com>
2024-02-09 00:29:08 +0200

    min and max sum of an int sequence

---
+++ DefiningClassesExerciseRerun/hackerrankPlusMinus/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> ints= Console.ReadLine().Split(",").Select(int.Parse).ToList();
+        miniMaxSum(ints);
+    }
+
+    public static void miniMaxSum(List<int> ints)
+    {
+        int minSum = int.MaxValue;
+        int maxSum = int.MinValue;
+        for (int i = 0; i < ints.Count; i++)
+        {
+            int sum = 0;
+            int removed = ints[0];
+            ints.RemoveAt(0);
+            foreach (int number in ints)
+            { 
+                sum += number;
+            }
+            if (sum > maxSum)
+            {
+                maxSum = sum;
+            }
+            else if (sum < minSum)
+            {
+                minSum = sum;
+            }
+            ints.Add(removed);
+        }
+        Console.WriteLine($"{minSum} {maxSum}");
+    }
+}
