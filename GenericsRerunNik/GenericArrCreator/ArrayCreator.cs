09844817a5210b27220809cf0ce985cb0098a599
marin <f0rc3ful@protonmail.com>
2024-02-11 21:29:50 +0200

    generic arr creator

---
+++ GenericsRerunNik/GenericArrCreator/ArrayCreator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArrCreator
+{
+    public class ArrayCreator<T>
+    {
+        public static T[] Creator(int length, T item)
+        {
+            T[] array = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = item;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ GenericsRerunNik/GenericArrCreator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArrCreator
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] array = ArrayCreator<string>.Creator(7,"Kolio");
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
