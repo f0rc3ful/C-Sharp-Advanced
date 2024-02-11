fb3fca11ce032886361d0dc700d4d77bfe0c1ca7
marin <f0rc3ful@protonmail.com>
2024-02-11 21:23:45 +0200

    box1, override ToString()

---
+++ GenericsRerunNik/GenericsRerunNik/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfString
+{
+    class Program
+    {
+        static void Main()
+        {
+            var box = new Box<string>();
+            box.Add("matrix");
+            box.Add("magicque");
+            Console.WriteLine(box.ToString());
+        }
+    }
+
+    class Box<T>
+    {
+        private List<T> internalList = new List<T>();
+
+        public void Add(T a)
+        {
+            internalList.Add(a);
+        }
+
+        public T Remove()
+        {
+            int length = internalList.Count;
+            T returned = internalList[length - 1];
+            internalList.RemoveAt(length - 1);
+            return returned;
+        }
+
+        public int Count
+        {
+            get 
+            {
+                return internalList.Count();
+            }
+        }
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {string.Join(", ", internalList)}";
+        }
+    }
+}
