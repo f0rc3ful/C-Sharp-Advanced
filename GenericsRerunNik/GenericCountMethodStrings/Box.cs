c45a16a22876b7cb9afc460929930330a145eab2
marin <f0rc3ful@protonmail.com>
2024-02-12 02:08:57 +0200

    s

---
+++ GenericsRerunNik/GenericCountMethodStrings/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCountMethodStrings
+{
+    public class Box<T>
+        where T : IComparable<T>
+    {
+        public List<T> box;
+
+        public Box()
+        {
+            box = new List<T>();
+        }
+
+        public void Counter(T item)
+        {
+            int counter = 0;
+            foreach (var element in box)
+            {
+                if (element.CompareTo(item) > 0)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ GenericsRerunNik/GenericCountMethodStrings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCountMethodStrings
+{
+    class Program
+    {
+        static void Main()
+        {
+            Box<string> list = new Box<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                list.box.Add(Console.ReadLine());
+            }
+
+            string item = Console.ReadLine();
+
+            list.Counter(item);
+        }
+
+        
+
+
+    }
+}
