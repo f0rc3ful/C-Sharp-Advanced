f822be0d8d20a413d48aa728c9ef260c62d13ce3
marin <f0rc3ful@protonmail.com>
2024-02-14 22:51:07 +0200

    into a single method

--- Hackerrank/BetweenTwoSetsOptimizedChatGPT/Program.cs
+++ Hackerrank/BetweenTwoSetsOptimizedChatGPT/Program.cs
@@ -7,31 +7,37 @@
         int output = BetweenTwoSets(a, b);
         Console.WriteLine(output);
     }
-    static int GCD(int a, int b)
-    {
-        while (b != 0)
-        {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
-
-    static int LCM(int a, int b)
-    {
-        return (a / GCD(a, b)) * b;
-    }
-
     static int BetweenTwoSets(List<int> a, List<int> b)
     {
-        int lcm = a.Aggregate((current, next) => LCM(current, next));
-        int gcd = b.Aggregate((current, next) => GCD(current, next));
-
-        int count = 0;
-        for (int i = lcm, step = lcm; i <= gcd; i += step)
+        // Local function to calculate the Greatest Common Divisor
+        int GCD(int x, int y)
         {
-            if (gcd % i == 0) count++;
+            while (y != 0)
+            {
+                int temp = y;
+                y = x % y;
+                x = temp;
+            }
+            return x;
+        }
+
+        // Local function to calculate the Least Common Multiple
+        int LCM(int x, int y) => (x / GCD(x, y)) * y;
+
+        // Calculate the LCM of all elements in list a
+        int totalLCM = a.Aggregate((result, next) => LCM(result, next));
+
+        // Calculate the GCD of all elements in list b
+        int totalGCD = b.Aggregate((result, next) => GCD(result, next));
+
+        // Count how many numbers are there between the LCM of a and the GCD of b
+        int count = 0;
+        for (int i = totalLCM; i <= totalGCD; i += totalLCM)
+        {
+            if (totalGCD % i == 0)
+            {
+                count++;
+            }
         }
 
         return count;
