3f4b5b847704d6f1ca1ed5656504849987a16344
marin <f0rc3ful@protonmail.com>
2024-02-17 11:20:16 +0200

    3. 92/100

---
+++ AdvancedModuleExam/3/Classifier.cs
@@ -0,0 +1,67 @@
+﻿namespace SharkTaxonomy
+{
+    public class Classifier
+    {
+		private int capacity;
+
+		public int Capacity
+		{
+			get { return capacity; }
+			set { capacity = value; }
+		}
+		private List<Shark> species;
+
+		public List<Shark> Species
+		{
+			get { return species; }
+			set { species = value; }
+		}
+
+		public int GetCount
+		{
+            get { return Species.Count(); }
+        }
+
+		public Classifier(int capacity)
+		{
+			Species = new List<Shark>();
+			Capacity = capacity;
+		}
+		public void AddShark(Shark shark)
+		{
+			if (Species.Count + 1 <= capacity)
+			{
+                if (!Species.Contains(shark))
+                {
+                    Species.Add(shark);
+                }
+            }
+		}
+		public bool RemoveShark(string kind)
+		{
+			int index = Species.FindIndex(shark => shark.Kind == kind);
+			if (index != -1)
+			{ 
+				Species.RemoveAt(index);
+				return true;
+			}
+
+			return false;
+		}
+
+		public string GetLargestShark()
+		{
+            Shark longestShark = Species.OrderByDescending(shark => shark.Length).FirstOrDefault();
+			return longestShark.ToString();
+		}
+		public int GetAverageLength()
+		{
+			int averageLength = Species.Sum(shark => shark.Length) / Species.Count();
+			return averageLength;
+		}
+		public string Report()
+		{
+			return $"{species.Count} sharks classified:\n{string.Join("\n", Species)}";
+		}
+	}
+}
---
+++ AdvancedModuleExam/3/Shark.cs
@@ -0,0 +1,45 @@
+﻿namespace SharkTaxonomy
+{
+    public class Shark
+    {
+		private string kind;
+
+		public string Kind
+		{
+			get { return kind; }
+			set { kind = value; }
+		}
+		private int length;
+
+		public int Length
+		{
+			get { return length; }
+			set { length = value; }
+		}
+		private string food;
+
+		public string Food
+		{
+			get { return food; }
+			set { food = value; }
+		}
+		private string habitat;
+
+		public string Habitat
+		{
+			get { return habitat; }
+			set { habitat = value; }
+		}
+		public Shark(string kind, int length, string food, string habitat)
+		{
+			Kind = kind;
+			Length = length;
+			Food = food;
+			Habitat = habitat;
+		}
+        public override string ToString()
+        {
+            return $"{kind} shark: {length}m long.\nCould be spotted in the {habitat}, typical menu: {food}";
+        }
+    }
+}
